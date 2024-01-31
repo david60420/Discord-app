f5f282b4b22af7f737a1a816a78b60add22d7faf
Ayprusss <theant741@gmail.com>
2024-01-30 20:03:41 -0500

    added themeProvider button for light and dark themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -15,8 +17,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={ cn(
+        font.className, 
+        "bg-white dark:bg-[#313338]"
+        )}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="discord-clone-theme"
+          >
+          {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
